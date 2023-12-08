6e42481acdf8516670f391ba04a108c10ae75870
mehdi bouziane <b.bouziane92@gmail.com>
2023-12-08 11:15:05 +0100

    correction de like en user_like

--- api/src/main/java/fr/mybodydate/registelogin/api/model/Like.java
+++ api/src/main/java/fr/mybodydate/registelogin/api/model/Like.java
@@ -5,8 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "user_like")
 public class Like {
 
     @Id
